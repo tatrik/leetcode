a3f7f8f577884a1cc2c986a5f36baf31809321a1
tatrik <tatrik9@gmail.com>
2024-01-24 19:36:45 +0100

    improve longest palindrome solution

--- longest_palindrome.py
+++ longest_palindrome.py
@@ -23,18 +23,15 @@ class Solution:
         start, end = 0, 0
 
         def expand_around_center(left: int, right: int) -> Tuple[int, int]:
+            nonlocal start, end
             while left >= 0 and right < len(s) and s[left] == s[right]:
+                if (right - left) > (end - start):
+                    start, end = left, right
                 left -= 1
                 right += 1
-            return left + 1, right - 1
 
         for i in range(len(s)):
-            left1, right1 = expand_around_center(i, i)
-            left2, right2 = expand_around_center(i, i+1)
-
-            if right1 - left1 > end - start:
-                start, end = left1, right1
-            if right2 - left2 > end - start:
-                start, end = left2, right2
+            expand_around_center(i,i)
+            expand_around_center(i,i+1)
 
         return s[start:end+1]
